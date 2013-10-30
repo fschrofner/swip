e20b3a24474441da11a72b14c4f09e9f4ae17bf1
Florian Schrofner <florian.schrofner@outlook.com>
2013-10-30 20:10:08 +0100

    first commit, containing the basic version of the app no automatic profile switching functionality

---
+++ src/src/at/fhhgbg/mc/profileswitcher/BootCompletedReceiver.java
@@ -0,0 +1,29 @@
+package at.fhhgbg.mc.profileswitcher;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * BootCompletedReceiver which starts the AutostartService to show the permanent
+ * notification on reboot.
+ * 
+ * @author Florian Schrofner & Dominik Koeltringer
+ * 
+ */
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+	/**
+	 * Starts the AutostartService when a it receives a boot completed message.
+	 * 
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
+	 *      android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context _context, Intent _intent) {
+		Log.i("BootCompletedReceiver", "boot completed");
+		Intent intent = new Intent(_context, AutostartService.class);
+		_context.startService(intent);
+	}
+}
