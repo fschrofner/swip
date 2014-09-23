84fc63f7dfa667749a4d338a1f3f4b24a01623f8
Dominik Koeltringer <dkoeltringer@users.noreply.github.com>
2014-09-23 19:13:49 +0200

    added missing files

---
+++ src/src/at/fhhgb/mc/swip/services/AutostartService.java
@@ -0,0 +1,47 @@
+package at.fhhgb.mc.swip.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+/**
+ * Service used to start the notification, if the permanent notification option
+ * is activated.
+ * 
+ * @author Florian Schrofner & Dominik Koeltringer
+ * 
+ */
+public class AutostartService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+
+	/**
+	 * Builds and shows the notification, when the service is started and the
+	 * permanent notification is enabled inside the settings.
+	 * 
+	 * @see android.app.Service#onStartCommand(android.content.Intent, int, int)
+	 */
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		SharedPreferences pref = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Log.i("AutostartService", "checking if notification is enabled");
+
+		// checks if the permanent notification option is enabled
+		if (pref.getBoolean("notification", false)) {
+			Handler handler = new Handler(this);
+			handler.updateNotification();
+		}
+		// starts the trigger service
+		Intent triggerIntent = new Intent(getApplicationContext(), at.fhhgb.mc.swip.trigger.TriggerService.class);
+		startService(triggerIntent);
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+}
